77e763fea707c2f2b06d2f429d03f2e5158af1ca
ultraspinx <58871991+ultraspinx@users.noreply.github.com>
2019-12-22 21:21:47 +0700

    Update head_left.php

--- res/templates/teacher/head_left.php
+++ res/templates/teacher/head_left.php
@@ -21,7 +21,7 @@
 	<div class="navbar-fixed">
 		<nav>
 			<div class="nav-wrapper nav-green">
-				<div class="left pad-left-20"><a class="cursor" id="trigger-sidebar"><i class="material-icons" id="menu-icon" title="Ẩn/Hiện bảng điều hướng">menu</i></a></div>
+				<div class="left pad-left-20"><a class="cursor" id="trigger-sidebar"><i class="material-icons" id="menu-icon" title="Show/hide directory board">menu</i></a></div>
 				<a href="index.php?action=show_dashboard" class="brand-logo right cursor"><i class="material-icons">home</i></a>
 			</div>
 		</nav>
@@ -39,24 +39,24 @@
 		<ul class="collapsible reset-margin" data-collapsible="accordion">
 			<li class="active">
 				<div class="collapsible-header" id="menu"><i class="material-icons hide" id="menu-arrow-down">arrow_drop_down</i>
-					<i class="material-icons" id="menu-arrow-up">arrow_drop_up</i>Danh Sách Lớp</div>
+					<i class="material-icons" id="menu-arrow-up">arrow_drop_up</i>Class list</div>
 					<div class="collapsible-body list" id="list_classes_sidebar">
 					</div>
 				</li>
-				<a href="index.php?action=list_test" class="collapsible-header font-color cursor"><i class="material-icons">list</i>Xem Danh Sách Điểm</a>
-				<a href="index.php?action=show_notifications" class="collapsible-header font-color cursor"><i class="material-icons">send</i>Gửi/Xem Thông Báo</a>
-				<a href="index.php?action=show_about" class="collapsible-header font-color cursor"><i class="material-icons">insert_comment</i>Liên Hệ</a>
+				<a href="index.php?action=list_test" class="collapsible-header font-color cursor"><i class="material-icons">list</i>See mark list</a>
+				<a href="index.php?action=show_notifications" class="collapsible-header font-color cursor"><i class="material-icons">send</i>Show notification</a>
+				<a href="index.php?action=show_about" class="collapsible-header font-color cursor"><i class="material-icons">insert_comment</i>How about</a>
 			</ul>
 		</div>
-		<a data-target="modal1" class="sidebar-show logout modal-trigger waves-effect" id="logout">Đăng Xuất</a>
+		<a data-target="modal1" class="sidebar-show logout modal-trigger waves-effect" id="logout">Log out</a>
 		<div id="modal1" class="modal">
 			<div class="modal-content">
 				<h4>Đăng Xuất</h4>
 				<p>Xác nhận đăng xuất</p>
 			</div>
 			<div class="modal-footer">
-				<a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Không</a>
-				<a class="modal-action modal-close waves-effect waves-green btn-flat" id="btn-logout">Có</a>
+				<a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">No </a>
+				<a class="modal-action modal-close waves-effect waves-green btn-flat" id="btn-logout">Yes</a>
 			</div>
 		</div>
 		<div class="box-content right box-content-mini" id="box-content">
